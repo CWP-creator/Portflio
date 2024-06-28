f60b1c1534396d42eb514fe02925ec8ec374745b
Pratyush Dulal <dulalpratyush@gmail.com>
2024-06-28 14:10:43 +0545

    changedd

--- assets/js/fullpage.js
+++ assets/js/fullpage.js
@@ -35,7 +35,7 @@ function myfunction() {
         }
         navElement.style.backgroundColor = "white";
         navElement.style.boxShadow = "0 4px 30px rgba(0, 0, 0, 0.2)";
-        imgElement.src = "../assets/img/dark.webp";
+        imgElement.src = "./assets/img/dark.webp";
         bodyElement.style.backgroundColor = "#f0f8ff";
         aboutElement.style.backgroundColor = "#fbfbfb";
         aboutElement.style.boxShadow = "0 4px 30px rgba(0, 0, 0, 0.4)";
@@ -91,7 +91,7 @@ function myfunction() {
         }
         navElement.style.backgroundColor = "#101720";
         navElement.style.boxShadow = "0 4px 30px rgba(255, 255, 255, 0.15)";
-        imgElement.src = "../assets/img/light3.png";
+        imgElement.src = "./assets/img/light3.png";
         bodyElement.style.backgroundColor = "#0b1215";
         aboutElement.style.backgroundColor = "#101720";
         aboutElement.style.boxShadow = "0 4px 30px rgba(255, 255, 255, 0.15)";
